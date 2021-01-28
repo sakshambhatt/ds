7291b8a43916c7ade220b9a19613fa07988a776e
Saksham Bhatt <sakshambhatt33@gmail.com>
2021-01-28 22:52:23 +0530

    firstAndLastDigitSum 2ndTry

--- CodeChef/src/beginner/firstAndLastDigit/Codechef.java
+++ CodeChef/src/beginner/firstAndLastDigit/Codechef.java
@@ -4,59 +4,54 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Codechef {
-
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		InputStreamReader r = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(r);
-		
-		int testCases = 0;
+		int numberOfInputs = 0;
 		try {
-			testCases = Integer.parseInt(br.readLine());
+			numberOfInputs = Integer.parseInt(br.readLine());
 		} catch (NumberFormatException | IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		int arr[] = new int[testCases];
 		int i = 0;
-		int num = 0;
 		int j = 0;
-		for(i = 0; i < testCases; i++) {
-			
+		int sum = 0;
+		int arr[] = new int[numberOfInputs];
+		for(i = 0; i < arr.length; i++) {
+			sum = 0;
 			try {
-				num = Integer.parseInt(br.readLine());
+				arr[i] = Integer.parseInt(br.readLine());
 			} catch (NumberFormatException | IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			
-			//adding first digit
-			arr[i] = arr[i] + num % 10;
-			
-			
 			//adding last digit
-			for(j = 0; j < 6; j++) {
+			sum = arr[i] % 10;
+			
+			//adding first digit
+			for(j = 1; j <= 6; j++) {
 				
-				if(num <= 9) {
-					
+				if(arr[i] <= 9) {
+					arr[i] = sum;
 					break;
 				}
-				else if(num / (int)Math.pow(10.0, (j + 1) * 1.0) <= 9 ) {
+				else if(arr[i] / (int)Math.pow(10 * 1.0, j * 1.0) <= 9) {
 					
-					arr[i] = arr[i] + num / (int)Math.pow(10.0, (j + 1) * 1.0);
+					sum = sum + arr[i] / (int)Math.pow(10 * 1.0, j * 1.0);
+					arr[i] = sum;
 					break;
 				}
 			}
-			
 		}
-		for(i = 0; i < testCases; i++) {
-			
+		for(i = 0; i < arr.length; i++) {
 			System.out.println(arr[i]);
 		}
-		
-		
 	}
-
 }
